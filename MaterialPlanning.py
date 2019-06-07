58795aa30d6ab7b80823cced40c662ee8a7de50f
ycremar <ethan.ycx@gmail.com>
2019-06-07 10:51:04 -0700

    item filters added

--- MaterialPlanning.py
+++ MaterialPlanning.py
@@ -193,14 +193,14 @@ class MaterialPlanning(object):
         for i,t in enumerate(n_looting):
             if t > 1.0:
                 target_items = np.where(self.probs_matrix[i]*t>=1)[0]
-                display_lst = [self.item_array[idx]+' (%d)'%int(self.probs_matrix[i, idx]*t) for idx in target_items]
+                display_lst = [self.item_array[idx]+'(%d)'%int(self.probs_matrix[i, idx]*t) for idx in target_items]
                 print('Stage ' + self.stage_array[i] + ' (%d times) ===> '%int(t) + ', '.join(display_lst))
         print('Synthesize following items:')
         for i,t in enumerate(n_convertion):
             if t > 1.0:
                 target_item = self.item_array[np.argmax(self.convertion_matrix[i])]
-                display_lst = [k+' (%d) '%(v*int(t)) for k,v in self.convertions_dct[target_item].items()]
-                print(target_item + ' (%d) <=== '%int(t) +  ', '.join(display_lst))
+                display_lst = [k+'(%d) '%(v*int(t)) for k,v in self.convertions_dct[target_item].items()]
+                print(target_item + '(%d) <=== '%int(t) +  ', '.join(display_lst))
 
 def Cartesian_sum(arr1, arr2):
     arr_r = []
