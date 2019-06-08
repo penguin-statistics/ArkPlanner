110ed8bde69686fcc2364d1b7553abf0db64321d
invisiblearts <invisiblearts@outlook.com>
2019-06-07 19:36:52 -0500

    fix encoding issue on Windows

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import sys
+import sys, codecs
 from MaterialPlanning import MaterialPlanning
 
 if __name__ == '__main__':
     
     mp = MaterialPlanning()
     
-    with open('required.txt', 'r') as f:
+    with codecs.open('required.txt', 'r', 'utf-8') as f:
         required_dct = {}
         for line in f.readlines():
             required_dct[line.split(' ')[0]] = int(line.split(' ')[1])
             
-    with open('owned.txt', 'r') as f:
+    with codecs.open('owned.txt', 'r', 'utf-8') as f:
         owned_dct = {}
         for line in f.readlines():
             owned_dct [line.split(' ')[0]] = int(line.split(' ')[1])
